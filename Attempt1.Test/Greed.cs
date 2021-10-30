9dceb667cdadb279dc11566fc5406f279c8de201
kamrul.islam <kamrul.islam@dotnet-specialist.co.uk>
2021-10-30 22:39:36 +0100

    All parts done, now for refactoring

--- Attempt1.Test/Greed.cs
+++ Attempt1.Test/Greed.cs
@@ -8,15 +8,38 @@ namespace Attempt1.Test
 {
     public class Greed
     {
-        private const int TRIPLE_APPEARANCE = 3;
+
         private const int ONE_APPEARANCE = 1;
+        private const int TRIPLE_APPEARANCE = 3;
+        private const int QUAD_APPERANCE = 4;
+
         private const int TRIPLE_SCORE_MULTIPLER = 100;
+        private const int QUAD_SCORE_MULTPLIER = 200;
+        private const int FIFTH_APPEARANCE = 5;
+        private const int FIFTH_SCORE_MULTPLIER = 400;
+        private const int SIXTH_APPEARANCE = 6;
+        private const int SIXTH_SCORE_MULTIPLIER = 800;
         private static readonly int[] diceRollsWithTripleAndSingleScore = new int[] { 1, 5 };
         private static readonly int[] diceRollWithTripleScoresOnly = new int[] { 2, 3, 6 };
 
         internal static int GetScore(int[] dices)
         {
             int score = 0;
+
+            var is3Pairs = dices.GroupBy(x => x)
+                                .Count(g => g.Count() == 2) == 3;
+            if (is3Pairs)
+            {
+                return 800;
+            }
+
+            var numbers1To10 = Enumerable.Range(1, 6);
+
+            if (dices.SequenceEqual(numbers1To10))
+            {
+                return 1200;
+            }
+
 
             foreach (var diceRollWithTripleAndSingleScores in diceRollsWithTripleAndSingleScore)
             {
@@ -26,20 +49,35 @@ namespace Attempt1.Test
 
             foreach (var diceRollWithTripleScoresOnly in diceRollWithTripleScoresOnly)
             {
-                score = ScoreTripleDiceRolls(dices, score, diceRollWithTripleScoresOnly);
+                score = ScoreMuliplierByNoAppearances(dices, score, diceRollWithTripleScoresOnly);
             }
+
+                   
+
 
 
             return score;
 
         }
-        private static int ScoreTripleDiceRolls(int[] dices, int score, int diceRoll)
+        private static int ScoreMuliplierByNoAppearances(int[] dices, int score, int diceRoll)
         {
-            var noDiceRolls = CountNoTime(dices, diceRoll);
-            if (noDiceRolls >= TRIPLE_APPEARANCE)
+            var countSameNoAppearance = CountNoTime(dices, diceRoll);
+            if (countSameNoAppearance == TRIPLE_APPEARANCE)
             {
                 score += diceRoll * TRIPLE_SCORE_MULTIPLER;
 
+            }
+            if (countSameNoAppearance == QUAD_APPERANCE)
+            {
+                score += diceRoll * QUAD_SCORE_MULTPLIER;
+            }
+            if (countSameNoAppearance == FIFTH_APPEARANCE)
+            {
+                score += diceRoll * FIFTH_SCORE_MULTPLIER;
+            }
+            if (countSameNoAppearance == SIXTH_APPEARANCE)
+            {
+                score += diceRoll * SIXTH_SCORE_MULTIPLIER;
             }
 
             return score;
@@ -63,6 +101,8 @@ namespace Attempt1.Test
                 score += diceRollMultiplier * TRIPLE_SCORE_MULTIPLER;
                 score += (noDiceRolls - TRIPLE_APPEARANCE) * diceRollSingleScoreMultiplier;
             }
+
+
 
             return score;
         }
--- Attempt1.Test/GreedTest.cs
+++ Attempt1.Test/GreedTest.cs
@@ -11,16 +11,13 @@ namespace Attempt1.Test
         [InlineData(new[] { 0, 1, 0, 3, 9 }, 100)]
 
         [InlineData(new[] { 0, 2, 0, 2, 2}, 200)]
-        [InlineData(new[] { 2, 2, 2, 2, 2}, 200)]
 
         [InlineData(new[] { 0, 3, 0, 3, 3}, 300)]
-        [InlineData(new[] { 3, 3, 3, 3, 3}, 300)]
 
         [InlineData(new[] { 0, 0, 5, 3, 9}, 50)]
         [InlineData(new[] { 0, 0, 5, 5, 5}, 500)]
 
         [InlineData(new[] { 6, 6, 6, 2, 2 }, 600)]
-        [InlineData(new[] { 6, 6, 6, 6, 6}, 600)]
         public void ShouldReturnSingleOrTripleScore(
             int[] dices, int expectedScore)
         {
@@ -45,7 +42,7 @@ namespace Attempt1.Test
 
         [Theory]
         [InlineData(new int[] {1}, 100)]
-        [InlineData(new int[] {1,2,3,4,5,6}, 150)]
+        [InlineData(new int[] {1,2,3,4,5,2}, 150)]
         public void ShouldReturnScoreFor1to6DiceThrown(
             int[] dices, int expectedScore)
         {
@@ -55,13 +52,37 @@ namespace Attempt1.Test
 
         }
 
-        [Fact]
-        public void ShouldReturnScoreFourOfAKind()
+        [Theory]
+        [InlineData(new int[] { 2, 2, 2, 2 }, 400)]
+        [InlineData(new int[] { 2, 2, 2, 2, 2 }, 800)]
+        [InlineData(new int[] { 2, 2, 2, 2, 2, 2 }, 1600)]
+        public void ShouldReturnScoredByMultiplierForKind(
+                        int[] dices, int expectedScore)
         {
-            int[] dices = new int[] { 2, 2, 2, 2 };
             var result = Greed.GetScore(dices);
 
-            Assert.Equal(400, result);
+            Assert.Equal(expectedScore, result);
+        }
+
+        [Fact]
+        public void ShouldReturnScoreThreePairs()
+        {
+            var dices = new int[] { 2, 2, 3, 3, 4, 4 };
+
+            var result = Greed.GetScore(dices);
+
+            Assert.Equal(800, result);
+        }
+
+
+        [Fact]
+        public void ShouldReturnScoreForStraight()
+        {
+            var dices = new int[] { 1, 2, 3, 4, 5, 6 };
+
+            var result = Greed.GetScore(dices);
+
+            Assert.Equal(1200, result);
         }
 
 
